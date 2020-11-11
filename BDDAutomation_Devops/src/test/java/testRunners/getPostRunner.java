16291c272072b92736e2a96130d305137d9574ab
RyanHipolito <67422622+RyanHipolito@users.noreply.github.com>
2020-11-11 20:04:47 +0800

    Update getPostRunner.java

--- BDDAutomation_Devops/src/test/java/testRunners/getPostRunner.java
+++ BDDAutomation_Devops/src/test/java/testRunners/getPostRunner.java
@@ -9,6 +9,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 		format = {"pretty", "html:test-outout"},
 		dryRun = false, //always set as false to execute the test steps
 		monochrome = true // generates console output into a readable format
+//Our Devops Initiative
 		)
 
 public class getPostRunner extends AbstractTestNGCucumberTests {
